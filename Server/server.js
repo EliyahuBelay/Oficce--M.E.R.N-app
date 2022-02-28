55557eea010a8521612e070b6447dda24870bbcc
eliyahubelay0@gmail.com <eliyahubelay0@gmail.com>
2022-02-28 17:02:00 +0200

    change order in server.js file

--- Server/server.js
+++ Server/server.js
@@ -15,20 +15,20 @@ require('./DB/index')
 
 const cors = require('cors');
 
+const workerRouter = require('./Routing/workersRout');
+
 const app = express();
+
+app.use(express.json());
+
+app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT);
 
-app.use(express.json());
-app.use(cors());
-
-
-const workerRouter = require('./Routing/workersRout');
-
-app.use('/worker',workerRouter);
-
 app.get('/',(request,response)=>{
     response.send({serverIsOnLine:true})
 })
+
+app.use('/worker',workerRouter);
